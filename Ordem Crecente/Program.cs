47d3905febb1942411f089f5948d1f5273ac590b
Paola-Santanna <paolinhasj@gmail.com>
2024-04-23 09:46:35 -0300

    Add project files.

---
+++ Ordem Crecente/Program.cs
@@ -0,0 +1,4 @@
+﻿// 3. Faça um programa que leia 5 números, guarde-os no vetor e ordene-os de forma crescente em um novo vetor;
+
+Console.WriteLine("\nAperte ENTER para encerrar...");
+Console.ReadKey();
